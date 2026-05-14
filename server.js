2e34514b28102135a9853edd1aadc7b19d97b105
poojasaini245304-glitch <poojasaini245304@gmail.com>
2026-05-14 17:26:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,73 +2,88 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
-  cors: { origin: "*" }
+    cors: { origin: "*", methods: ["GET", "POST"] }
 });
 
-const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 let players = {};
-let obstacles = [];
-let coins = [];
+let chatMessages = [];
 
 io.on('connection', (socket) => {
-  console.log('Player joined:', socket.id);
-  
-  socket.on('joinGame', (playerName) => {
-    if(Object.keys(players).length >= 5) {
-      socket.emit('roomFull');
-      return;
-    }
-    
-    const playerColors = ['#00d4ff','#ff0066','#00ff88','#9d4edd','#ffd700'];
-    const playerLanes = [125, 275, 425, 575, 725];
-    const playerIndex = Object.keys(players).length;
-    
-    players[socket.id] = {
-      id: socket.id,
-      name: playerName || `Player ${playerIndex + 1}`,
-      x: playerLanes[playerIndex],
-      lane: playerIndex,
-      score: 0,
-      color: playerColors[playerIndex],
-      speed: 2.5
-    };
-    
-    socket.emit('initGame', players[socket.id]);
-    io.emit('updatePlayers', players);
-    console.log('Players online:', Object.keys(players).length);
-  });
-  
-  socket.on('movePlayer', (data) => {
-    if(players[socket.id]) {
-      players[socket.id].lane = data.lane;
-      players[socket.id].x = data.x;
-      players[socket.id].score = data.score;
-      players[socket.id].speed = data.speed;
-      socket.broadcast.emit('playerMoved', players[socket.id]);
-    }
-  });
-  
-  socket.on('spawnObstacle', (obs) => {
-    socket.broadcast.emit('newObstacle', obs);
-  });
-  
-  socket.on('spawnCoin', (coin) => {
-    socket.broadcast.emit('newCoin', coin);
-  });
-  
-  socket.on('chatMessage', (msg) => {
-    io.emit('newChat', { name: players[socket.id]?.name || 'Player', msg: msg });
-  });
-  
-  socket.on('disconnect', () => {
-    console.log('Player left:', socket.id);
-    delete players[socket.id];
-    io.emit('updatePlayers', players);
-  });
+    console.log('Player connected:', socket.id);
+
+    socket.on('joinGame', (data) => {
+        players[socket.id] = {
+            id: socket.id,
+            name: data.name,
+            x: 0,
+            y: 0,
+            rotation: 0,
+            carType: data.carType || 'f1',
+            lap: 0
+        };
+        io.emit('updatePlayers', players);
+        socket.emit('chatHistory', chatMessages);
+    });
+
+    socket.on('playerMove', (data) => {
+        if (players[socket.id]) {
+            players[socket.id].x = data.x;
+            players[socket.id].y = data.y;
+            players[socket.id].rotation = data.rotation;
+            players[socket.id].lap = data.lap;
+            io.emit('updatePlayers', players);
+        }
+    });
+
+    socket.on('chatMessage', (data) => {
+        chatMessages.push(data);
+        if (chatMessages.length > 50) chatMessages.shift();
+        io.emit('chatMessage', data);
+    });
+
+    socket.on('honk', (name) => {
+        io.emit('honk', name);
+    });
+
+    // VOICE CHAT
+    socket.on('startVoice', (name) => {
+        socket.broadcast.emit('startVoice', name);
+    });
+
+    socket.on('stopVoice', (name) => {
+        socket.broadcast.emit('stopVoice', name);
+    });
+
+    socket.on('speaking', (data) => {
+        socket.broadcast.emit('speaking', data);
+    });
+
+    socket.on('offer', (data) => {
+        socket.to(data.to).emit('offer', { from: socket.id, offer: data.offer });
+    });
+
+    socket.on('answer', (data) => {
+        socket.to(data.to).emit('answer', { from: socket.id, answer: data.answer });
+    });
+
+    socket.on('ice-candidate', (data) => {
+        socket.to(data.to).emit('ice-candidate', { from: socket.id, candidate: data.candidate });
+    });
+
+    socket.on('raceWon', (name) => {
+        io.emit('chatMessage', { name: 'SYSTEM', message: 🏆 ${name} WON THE RACE! 🏆 });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Player disconnected:', socket.id);
+        delete players[socket.id];
+        io.emit('updatePlayers', players);
+    });
 });
 
+const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-  console.log(`🏁 Qubenexus Racing Server running on port ${PORT}`);
+    console.log(Server running on port ${PORT});
 });
